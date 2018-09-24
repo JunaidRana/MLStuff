cc15cf35961a4ad69caf69bcd8bf57cf569d44b0
Junaid Rana <junaidraza52@gmail.com>
2018-09-24 16:27:33 +0500

    Sample File

---
+++ Regression/model.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 24 11:27:54 2018
+
+@author: Junaid.raza
+"""
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import re
+import time
+tf.__version__
+
+print ("Hello World")
